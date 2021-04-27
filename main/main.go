a375d2106c02c72e366d38259bd1409b857e97a5
Safwan Shaheer <devorein00@gmail.com>
2021-04-27 18:24:15 +0600

    Sum of odd numbers

--- main/main.go
+++ main/main.go
@@ -7,12 +7,13 @@ import (
 )
 
 func main() {
-	fmt.Println(codewars.RepeatStr(5, "A"))
-	fmt.Println(codewars.IsTriangle1(5, 1, 2))
-	fmt.Println(codewars.IsTriangle2(5, 1, 2))
-	fmt.Println(codewars.AbbrevName1("Sam Harris"))
-	fmt.Println(codewars.AbbrevName2("Sam Harris"))
-	fmt.Println(codewars.AbbrevName3("Sam Harris"))
-	fmt.Println(codewars.AbbrevName4("Sam Harris"))
-	fmt.Println(codewars.FindOdd([]int{20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5}))
+	// fmt.Println(codewars.RepeatStr(5, "A"))
+	// fmt.Println(codewars.IsTriangle1(5, 1, 2))
+	// fmt.Println(codewars.IsTriangle2(5, 1, 2))
+	// fmt.Println(codewars.AbbrevName1("Sam Harris"))
+	// fmt.Println(codewars.AbbrevName2("Sam Harris"))
+	// fmt.Println(codewars.AbbrevName3("Sam Harris"))
+	// fmt.Println(codewars.AbbrevName4("Sam Harris"))
+	// fmt.Println(codewars.FindOdd([]int{20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5}))
+	fmt.Println(codewars.RowSumOddNumbers(13))
 }
